81dff09b3b099081f418c92256d0928e1ca701a1
Dan Popa <Dan Popa>
2017-01-14 18:01:16 +0200

    Step 4-5

--- source/Source.c
+++ source/Source.c
@@ -127,7 +127,82 @@ int main(int argc, char *argv[]) {
 	des_step3_stop=MPI_Wtime();
 	//step3
 	
+	if(rank==0)
+	{
+		FILE *ed=fopen("input.des","w");
+		fwrite(master_buffer,1,length,ed);
+		fclose(ed);	
+
+		RSA *pair =  RSA_new();
+		pair=RSA_generate_key(1024,17 , NULL, NULL);
+		BIO *bio=BIO_new_file("file.prvkey","w+");
+		PEM_write_bio_RSAPrivateKey(bio,pair,NULL,NULL,0,NULL,NULL);
+		BIO_free(bio);
+		bio=BIO_new_file("file.pubkey","w+");
+		PEM_write_bio_RSAPublicKey(bio,pair);
+		BIO_free(bio);
+		RSA_free(pair);
+	}
+
+	printf ("Step4 rank= %d \n", rank);
+	//step4 - RSA encryption
+	MPI_Barrier(MPI_COMM_WORLD);
+	rsa_step4_start=MPI_Wtime();
+
+	MPI_Scatter(master_buffer,length/numtasks,MPI_CHAR,worker_buffer,length/numtasks,MPI_CHAR,0,MPI_COMM_WORLD);
+
+	RSA *pair=RSA_new();
+	BIO *bio=BIO_new_file("file.pubkey","r");
+	PEM_read_bio_RSAPublicKey(bio,&pair,NULL,NULL);
+	BIO_free(bio);
+	RSA_public_encrypt(length/numtasks,worker_buffer,worker_buffer,pair,RSA_PKCS1_PADDING);
+	RSA_free(pair);
+	MPI_Gather(worker_buffer,length/numtasks,MPI_CHAR,master_buffer,length/numtasks,MPI_CHAR,0,MPI_COMM_WORLD);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	rsa_step4_stop=MPI_Wtime();
+	//step4
 	
+	if(rank==0)
+	{
+		f=fopen("input.rsa","w");
+		fwrite(master_buffer,1,1024,f);
+		fclose(f);
+		SHA384_Init(&ctx2);
+		hashstring=(unsigned char*)malloc(48*numtasks);
+	}
+
+	printf ("Step5 rank= %d \n", rank);
+	//step5 - hash on cypher
+	MPI_Barrier(MPI_COMM_WORLD);
+	cypher_hash_step5_start=MPI_Wtime();
+
+	MPI_Scatter(master_buffer,length/numtasks,MPI_CHAR,worker_buffer,length/numtasks,MPI_CHAR,0,MPI_COMM_WORLD);
+
+	SHA512_CTX localctx2;
+	unsigned char hash2[48];
+	SHA384_Init(&localctx2);
+	SHA384_Update(&localctx2, worker_buffer, length/numtasks);
+	SHA384_Final(hash2, &localctx2);
+
+	MPI_Gather(hash2,48,MPI_CHAR,hashstring,48,MPI_CHAR,0,MPI_COMM_WORLD);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	cypher_hash_step5_stop=MPI_Wtime();
+	//step5
+	
+	if(rank==0)
+	{
+		SHA384_Update(&ctx,hashstring,48*numtasks);
+		SHA384_Final(finalhash,&ctx);
+		FILE *h2=fopen("cipher.hash","w");
+		fwrite(finalhash,1,48,h2);
+		fclose(h2);
+		FILE *g=fopen("Rezultate.txt","a");
+		fprintf(g,"%d - ",numtasks);
+		fprintf(g,"Step1 %lf sec - Step2 %lf sec - Step3 %lf sec - Step4 %lf sec - Step5 %lf sec\n",file_gen_step1_stop-file_gen_step1_start,plain_hash_step2_stop-plain_hash_step2_start,des_step3_stop-des_step3_start,rsa_step4_stop-rsa_step4_start,cypher_hash_step5_stop-cypher_hash_step5_start);
+		fclose(g);
+	}
 
 	MPI_Finalize();
 }
