8808eab01baeb5b4debf56dc331ac124a4241fdc
andreitalas <andreitalas@gmail.com>
2017-01-15 20:42:16 +0200

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,10 @@
+sudo apt-get install openmpi-bin openmpi-common openssh-client openssh-server libopenmpi-dev
+sudo apt-get install -y libssl-dev
 parallel-ssh -i -A -H "ub1 ub2" -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" echo hi
 parallel-ssh -i -A -H "ub1 ub2 ub3" -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo apt-get install -y nfs-client
 parallel-ssh -i -A -H "ub0 ub1 ub2 ub3" -l usr -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo mkdir /mirror
 echo "/mirror *(rw,sync)" | sudo tee -a /etc/exports
 parallel-ssh -i -A -H "ub1 ub2 ub3" -l usr -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo mount ub0:/mirror /mirror
 parallel-ssh -i -A -H "ub0 ub1 ub2 ub3" -l usr -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo newusers /mirror/userfile
-parallel-ssh -i -A -h /mirror/hosts -l usr -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo apt-get install -y mpich2
+parallel-ssh -i -A -h /mirror/hosts -l usr -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo apt-get install -y mpich
 parallel-ssh -i -A -h /mirror/hosts -l usr -x "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null" sudo apt-get install -y libssl-dev
